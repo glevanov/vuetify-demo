da4aeb328ad46487fd750b30552f9a772805ddb8
Grigoriy Levanov <glevanov@gmail.com>
2019-03-10 23:23:22 +0300

    Adds home details section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <span>
     <home-hero></home-hero>
+    <home-details></home-details>
   </span>
 </template>
 
 <script>
 import HomeHero from '@/components/HomeHero.vue';
+import HomeDetails from '@/components/HomeDetails.vue';
 
 export default {
   name: 'home',
   components: {
     HomeHero,
+    HomeDetails,
   },
 };
 </script>
