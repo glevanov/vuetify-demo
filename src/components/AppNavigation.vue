a6018f034273a37ace231e96c7697e5acc132e20
Grigoriy Levanov <glevanov@gmail.com>
2019-03-13 21:30:38 +0300

    Adds routes

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,27 +1,29 @@
 <template>
-<span>
-  <v-navigation-drawer app v-model="drawer" class="brown lighten-2" dark disable-resize-watcher>
-    <v-list>
-      <template v-for="(item, index) in items">
-        <v-list-tile :key="index">
-          <v-list-tile-content>
-            {{item.title}}
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-divider :key="`divider-${index}`"></v-divider>
-      </template>
-    </v-list>
-  </v-navigation-drawer>
-  <v-toolbar app color="brown darken-4" dark>
-    <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
-    <v-spacer class="hidden-md-and-up"></v-spacer>
-    <v-toolbar-title>{{appTitle}}</v-toolbar-title>
-    <v-btn flat class="hidden-sm-and-down">Menu</v-btn>
-    <v-spacer class="hidden-sm-and-down"></v-spacer>
-    <v-btn flat class="hidden-sm-and-down">SIGN IN</v-btn>
-    <v-btn color="brown lighten-3" class="hidden-sm-and-down">JOIN</v-btn>
-  </v-toolbar>
-</span>
+    <span>
+        <v-navigation-drawer app v-model="drawer" class="brown lighten-2" dark disable-resize-watcher>
+            <v-list>
+                <template v-for="(item, index) in items">
+                    <v-list-tile :key="index">
+                        <v-list-tile-content>
+                            {{item.title}}
+                        </v-list-tile-content>
+                    </v-list-tile>
+                    <v-divider :key="`divider-${index}`"></v-divider>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar app color="brown darken-4" dark>
+            <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
+            <v-spacer class="hidden-md-and-up"></v-spacer>
+            <router-link to="/">
+                <v-toolbar-title to="/">{{appTitle}}</v-toolbar-title>
+            </router-link>
+            <v-btn flat class="hidden-sm-and-down" to="/menu">Menu</v-btn>
+            <v-spacer class="hidden-sm-and-down"></v-spacer>
+            <v-btn flat class="hidden-sm-and-down" to="/sign-in">SIGN IN</v-btn>
+            <v-btn color="brown lighten-3" class="hidden-sm-and-down" to="/join">JOIN</v-btn>
+        </v-toolbar>
+    </span>
 </template>
 
 <script>
@@ -33,6 +35,7 @@ export default {
       drawer: false,
       items: [
         { title: 'Menu' },
+        { title: 'Profile' },
         { title: 'Sign In' },
         { title: 'Join' },
       ],
@@ -42,5 +45,8 @@ export default {
 </script>
 
 <style scoped>
-
+  a {
+    color: white;
+    text-decoration: none;
+  }
 </style>
